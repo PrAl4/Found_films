580f85fe30ab291cad0892a174164d1fe3e8f066
Екатерина Прищепа <78316829+ekaterina432@users.noreply.github.com>
2022-12-21 09:16:20 +0000

    исправление ошибки миграции

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_20_185801) do
+ActiveRecord::Schema.define(version: 2022_12_21_083950) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "questions", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -25,7 +31,6 @@ ActiveRecord::Schema.define(version: 2022_12_20_185801) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
-    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
